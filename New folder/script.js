d04242f590c46b2f4fc805a6b3947bba871cdbab
gutti kaveri <gutikaveri@gmail.com>
2024-07-03 19:37:14 +0530

    javascript ass

---
+++ New folder/script.js
@@ -0,0 +1,63 @@
+// 1. array initialization:
+// create an array of 10 random integers between 1 and 100
+//  let array = [10, 22, 15, 44,65, 6, 37,98, 99, 100];
+
+//2 push
+
+// . array.push(101);
+// console.log(array);
+
+// pop
+// array.pop();
+// console.log(array);
+
+// unshift
+// array.unshift(102);
+// console.log(array);
+
+// shift
+// array.shift();
+// console.log(array);
+
+
+
+// 3. Array sorting
+//  descending order:
+//  let sorted = array.sort((a, b) => b - a);
+//  console.log(sorted);
+
+//  asscending order:
+// sorted = array.sort((a, b) => a - b);
+// console.log(sorted);
+
+//4. foreach:
+
+// array.forEach((element) => {
+//   console.log(element);
+// });
+
+//5.filter:
+
+// let even = array.filter((even) => even % 2 === 0);
+// console.log(even);
+
+
+
+//6. map
+
+// let square= array.map((squares) => squares * 2);
+// console.log(square);
+
+
+
+// 7. reduce
+
+//  let sum = array.reduce(
+//   (previousValue, currentValue) => previousValue + currentValue,
+//   0
+//  );
+
+// console.log(sum);
+
+
+
