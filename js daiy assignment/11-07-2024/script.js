72f2322d407421051c4a69c8672913e357da81df
gutti kaveri <gutikaveri@gmail.com>
2024-07-11 18:36:22 +0530

    create, update, delete and append

--- js daiy assignment/11-07-2024/script.js
+++ js daiy assignment/11-07-2024/script.js
@@ -3,12 +3,12 @@
 const newDiv = document.createElement('div');
 newDiv.innerText = "Hello, World!";
 document.body.appendChild(newDiv);
-______________________________________________________________________________________________________________________
+// ______________________________________________________________________________________________________________________
 // task2:Chainging  the content of the elements:
 
 const para =document.querySelector('p');
 para.innerText="This is updated content";
-______________________________________________________________________________________________________________________
+// ______________________________________________________________________________________________________________________
 // task3:Creating and appending a list of items:
 
 const unlist = document.createElement('ul');
@@ -25,19 +25,37 @@ const li3 = document.createElement('li');
  unlist.append(li3);
 
  document.body.appendChild(unlist);
-_____________________________________________________________________________________________________________________
+// _____________________________________________________________________________________________________________________
  // task4:Editing an Attribute of an Element:
 
  const img = document.querySelector('img');
  img.src = ' https://www.mariefranceasia.com/wp-content/uploads/sites/7/2016/04/feature-copy-5-750x410.jpg ';
 
 
-______________________________________________________________________________________________________________________
+// ______________________________________________________________________________________________________________________
  // task5:Removing an Element:
 
  const renoving = document.querySelector('div');
  renoving.remove();
 
+// ------------------------------------------------------------------------------------------------------------------------
+            //    reflection questions:
+// 1. what did you learn  about DOM manipulation through this task?
+// I learned  how   to use  (document.querySelector )to select specific elements in the DOM and
+// creating new elements using the DOM.
+// appending, editing and removing elements using the DOM.
+
+// 2.how do you create and append an element in the DOM?
+
+// I created an element using the (createElement)method and 
+// appended it to the DOM using the (.append )method.
+
+
+// 3. what methodsar e used to change the content and attributes of an element?
+// I used the innerText and src methods to change the content and attributes of an element.
+
+//  4. what method is used to remove an element from the DOM?
+//  I used  .remove  to a particular element to remove an element from the DOM.
 
 
 
