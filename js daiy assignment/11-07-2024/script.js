75de3a80e506cf7db62a9039089eeb80028137da
gutti kaveri <gutikaveri@gmail.com>
2024-07-11 17:55:02 +0530

    creating,chainging,removing,editing  of dom

---
+++ js daiy assignment/11-07-2024/script.js
@@ -0,0 +1,47 @@
+// task1:creating and  appending an element:
+
+const newDiv = document.createElement('div');
+newDiv.innerText = "Hello, World!";
+document.body.appendChild(newDiv);
+______________________________________________________________________________________________________________________
+// task2:Chainging  the content of the elements:
+
+const para =document.querySelector('p');
+para.innerText="This is updated content";
+______________________________________________________________________________________________________________________
+// task3:Creating and appending a list of items:
+
+const unlist = document.createElement('ul');
+const li1 = document.createElement('li');
+const li2 = document.createElement('li'); 
+const li3 = document.createElement('li');
+
+ li1.innerText = "Item1";
+ li2.innerText = "Item2";
+ li3.innerText = "Item3";
+
+ unlist.append(li1);
+ unlist.append(li2);
+ unlist.append(li3);
+
+ document.body.appendChild(unlist);
+_____________________________________________________________________________________________________________________
+ // task4:Editing an Attribute of an Element:
+
+ const img = document.querySelector('img');
+ img.src = ' https://www.mariefranceasia.com/wp-content/uploads/sites/7/2016/04/feature-copy-5-750x410.jpg ';
+
+
+______________________________________________________________________________________________________________________
+ // task5:Removing an Element:
+
+ const renoving = document.querySelector('div');
+ renoving.remove();
+
+
+
+
+
+
+
+ 
